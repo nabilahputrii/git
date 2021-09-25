690567b366f446b1276b6e622294815c2313cfa5
nabilah putri <nblptr33@gmail.com>
2021-09-25 18:14:53 +0700

    praktikum nabilah putri

---
+++ untitled0.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 13:20:40 2021
+
+#Praktikum 1
+#materi variabel pemograman dasar
+#Nabilah Putri
+#065002100002
+"""
+
+print('nabilah putri')
+print('jalan usaha')
+no=29
+print(no)
+print('rt7')
+print('rw 10')
+print('kecamatan tanjung priok')
+print('jakarta utara')
+
+
+
---
+++ untitled1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 25 09:28:18 2021
+
+#Laporan praktikum 1
+# memasukkan/menginputkan panjang dan lebar dari sebuah ruangan
+#Nabilah Putri
+#065002100002
+
+"""
+
+# assigment statement
+
+y = input('siapa nama kamu?')
+panjang= float(input('kamar kamu panjangnya berapa meter?'))
+lebar = float(input('kamar kamu lebarnya berapa meter?'))
+L = panjang*lebar #ekspresi
+print('berapa jumlah luas kamar kamu?', str(L))
